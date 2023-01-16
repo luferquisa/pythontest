333f6065a1bd130fa4798703b7045c9e0c480b03
Ubuntu <ubuntu@ip-172-31-94-12.ec2.internal>
2023-01-16 02:38:44 +0000

    se agrega metodo para retornar valor ascii

--- playground.py
+++ playground.py
@@ -3,26 +3,29 @@ diferentes ejercicios de programacion"""
 
 from math import pi
 
-
+#Calcula el area de un circulo
 def areacirculo(r):
 	return pi*r**2
 
+#Convierte grados Farenheit a cenlcius
 def farenheitacelcius(f):
 	return (f-32)*(5/9)
 
+#Retorna la extension de un archivo
 def extensionarchivo(filename):
 	split = filename.split(".")
 	return split[-1]
 
+#Retorna is un numero dado es par o impar
 def esparoimpar(n):
 	mod = n % 2
 	if mod > 0:
 		return "impar"
+	else:
+		return "par"
 
 def valorascii(c):
 	return ord(c)
-	else:
-		return "par"
 
 if __name__ == "__main__":
 	print("hola")
